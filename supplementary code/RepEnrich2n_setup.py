855d1a3b13b3c5be2036e0b8c86f264053939d2c
TheFackelmayerLab <github@fackelmayer.de>
2024-06-02 13:28:54 +0300

    Add files via upload

--- supplementary code/RepEnrich2n_setup.py
+++ supplementary code/RepEnrich2n_setup.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python
 import argparse
 import csv
 import shlex
 import subprocess
 import sys
+import os
 from collections import defaultdict
+# from concurrent.futures import ProcessPoolExecutor   # not needed because my modified code runs sequentially
 
 
 from Bio import SeqIO
@@ -45,10 +48,15 @@ annotation_file = args.annotation_file
 genomefasta = args.genomefasta
 cpus = args.cpus
 
-# using your own path to save the data instead of cluttering my general user folder
-fofs_path = "/path/to/repenrich_intermediates/"
+# using my own path to save the data instead of cluttering my general user folder
+fofs_path = "/Users/fof/Desktop/repeat_analysis/repenrich_intermediates_censat/"
 # or leave that path empty
 #fofs_path = ""
+bt_path = os.path.join(fofs_path, "bt")
+
+if not os.path.exists(bt_path):
+   os.makedirs(bt_path)
+   print(f"Path '{bt_path}' created.")
 
 
 def starts_with_numerical(list):
@@ -81,10 +89,8 @@ for chr in g.keys():
 repeat_elements = set()
 rep_coords = defaultdict(list)  # Merged dictionary for coordinates
 
-with open(f"{fofs_path}repnames.bed", 'w') as fout:
-    f_in = import_text(annotation_file, '\t') 
-    # above: delimiter changed to tab '\t' because otherwise the resulting bed file is empty
-    # change this if your repnames.bed file isn't tab-delimited!
+with open(os.path.join(bt_path, "repnames.bed"), 'w') as fout:
+    f_in = import_text(annotation_file, '\t') # FOF: delimiter changed to tab
     for line in f_in:
         repname = line[9].translate(str.maketrans('()/', '___'))
         repeat_elements.add(repname)
@@ -96,7 +102,8 @@ with open(f"{fofs_path}repnames.bed", 'w') as fout:
 # containing chromosome, start, and end coordinates for each repeat instance
 
 # sort repeat_elements and print them in repeatIDs.txt
-with open(f"{fofs_path}repeatIDs.txt", 'w') as fout:
+
+with open(os.path.join(bt_path, "repeatIDs.txt"), 'w') as fout:
     for i, repeat in enumerate(sorted(repeat_elements)):
         fout.write('\t'.join([repeat, str(i)]) + '\n')
 
@@ -122,13 +129,37 @@ for repname in rep_coords:
     SeqIO.write(record, fastafilename, "fasta")
 
 
-# new code to execute the function sequentially, because parallel processing created errors
+# original code commented out because parallel processing created errors
+# I will try sequentially with my code below 
+#
+#def bowtie_build(args):
+#    """
+#    Function to be executed in parallel by ProcessPoolExecutor.
+#    """
+#    try:
+#        bowtie_base, fasta = args
+#        command = shlex.split(f"bowtie2-build -f {fasta} {bowtie_base}")
+#        squash = subprocess.run(command, capture_output=True, text=True)
+#        return squash.stdout
+#    except Exception as e:
+#        return str(e)
+#
+#
+#args_list = [(name, f"{fofs_path}{name}.fa") for name in rep_coords]
+#with ProcessPoolExecutor(max_workers=cpus) as executor:
+#    executor.map(bowtie_build, args_list)
+
+
+# my new code to execute the function sequentially
 def bowtie_build(args):
-  # Function to build a bowtie index and save it to fofs_path.
+  """
+  Function to build a bowtie index and save it to bt_path.
+  """
   try:
     bowtie_base, fasta = args
     # Modify output filename to include .bt2 extension
-    bowtie_out = f"{fofs_path}/bt/{bowtie_base}"
+#    bowtie_out = f"{bt_path}{bowtie_base}"
+    bowtie_out = os.path.join(bt_path, bowtie_base)
     command = shlex.split(f"bowtie2-build -f {fasta} {bowtie_out}")
     squash = subprocess.run(command, capture_output=True, text=True)
     return squash.stdout
@@ -141,4 +172,4 @@ args_list = [(name, f"{fofs_path}{name}.fa") for name in rep_coords]
 # Sequential execution
 for name, fasta_path in args_list:
   squash = bowtie_build((name, fasta_path))
-  # Do something additional with the output (squash.stdout) if you want (totally optional!)
+  # Do something with the output (squash.stdout) if needed
